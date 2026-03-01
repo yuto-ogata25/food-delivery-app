5bf6a2a262112807bff561ddda7991aa3d71d442
yuto-ogata25 <yuto.ogata25@gmail.com>
2026-03-01 22:39:52 +0900

    build restaurant-card design

--- src/components/restaurant-card.tsx
+++ src/components/restaurant-card.tsx
@@ -1,5 +1,27 @@
+import { Heart } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 export default function RestaurantCard() {
-  return <div className='bg-red-200'>RestaurantCard</div>
+  return (
+        <div className="relative">
+            <Link href={"/abc"} className='inset-0 absolute z-10'></Link>
+            <div className="relative aspect-video rounded-lg overflow-hidden">
+                <Image className="object-cover" src={"/no_image.png"} fill alt="restaurant image"/>
+            </div>
+            <div className="flex justify-between items-center">
+                <p className="font-bold">name</p>
+            <div className='z-20'>
+                <Heart
+                 color='gray'
+                 strokeWidth={3}
+                 size={15} 
+                 className='hover:fill-red-500 hover:stroke-0'/>
+            </div>
+            </div>
+
+        </div>
+  );
+
 }
